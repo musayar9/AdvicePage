7d039e048af45666aae54cb64e11432b66318a92
Musa Sayar <musasayar67@gmail.com>
2022-12-29 11:19:01 +0300

    AdvicePage

---
+++ script.js
@@ -0,0 +1,39 @@
+const adviceText =  document.querySelector(".advice")
+const adviceId = document.querySelector(".idNum")
+const btnRandom = document.querySelector(".btn")
+const sound = document.querySelector(".sound")
+const copy = document.querySelector(".copy")
+
+btnRandom.addEventListener("click", ()=>{
+ showAdvice()
+})
+
+async function  showAdvice() {
+
+    const url = "https://api.adviceslip.com/advice"
+
+    const response = await fetch(url)
+   
+    const data = await response.json()
+
+    const result = await data.slip.advice;
+    const Id = await data.slip.id
+
+
+
+    adviceText.innerText =  `"${result}"`
+    adviceId.innerHTML = `<span>#${Id}</span>`
+
+}
+
+sound.addEventListener("click", () =>{
+    let utterance ;
+    utterance = new SpeechSynthesisUtterance(adviceText.innerText)
+    speechSynthesis.speak(utterance)
+  })
+
+  copy.addEventListener("click",()=>{
+    navigator.clipboard.writeText(adviceText.innerText)
+  })
+
+
